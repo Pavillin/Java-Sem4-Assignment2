11c03d434ffb91ad3e85bf63b78938253fffdbb9
Dylan <dylansprague11@hotmail.com>
2018-11-28 20:28:03 -0500

    Updated Product class

--- src/Product.java
+++ src/Product.java
@@ -1,5 +1,7 @@
 import javafx.scene.image.Image;
 
+import java.util.Arrays;
+
 public class Product {
     private String title, rating, genre;
     private double price;
@@ -17,7 +19,14 @@ public class Product {
         return title;
     }
 
+    /**
+     * Verify title isn't empty
+     * @param title
+     */
     public void setTitle(String title) {
+        if (title.isEmpty()){
+            throw new IllegalArgumentException("Title cannot be empty.");
+        }
         this.title = title;
     }
 
@@ -25,24 +34,66 @@ public class Product {
         return rating;
     }
 
+    /**
+     * Verify rating isn't empty and matches one of the valid ratings
+     * @param rating
+     */
     public void setRating(String rating) {
-        this.rating = rating;
+        if (rating.isEmpty()){
+            throw new IllegalArgumentException("Rating cannot be empty.");
+        }
+        //Everyone, Everyone 10 and up, Teen, Mature, Adult Only, Rating Pending
+        String validRating[] = {"E", "E10+", "T", "M", "AO", "RP"};
+        for (int i = 0; i < validRating.length; i++){
+            if (rating.equalsIgnoreCase(validRating[i])){
+                this.rating = rating;
+                break;
+            }
+            if (i == validRating.length - 1){
+                throw new IllegalArgumentException("Rating must be one of the following: E, E10+, T, M, AO, or RP.");
+            }
+        }
     }
 
     public String getGenre() {
         return genre;
     }
 
+    /**
+     * Verify genre isn't empty and is a valid genre
+     * @param genre
+     */
     public void setGenre(String genre) {
-        this.genre = genre;
+        if (genre.isEmpty()){
+            throw new IllegalArgumentException("Genre cannot be empty.");
+        }
+        //Everyone, Everyone 10 and up, Teen, Mature, Adult Only, Rating Pending
+        String validGenres[] = {"Action", "Action-Adventure", "Adventure", "Role-Playing", "Simulation", "Strategy", "Sports", "MMO"};
+        for (int i = 0; i < validGenres.length; i++){
+            if (rating.equalsIgnoreCase(validGenres[i])){
+                this.genre = genre;
+                break;
+            }
+            if (i == validGenres.length - 1){
+                throw new IllegalArgumentException("Genre must be one of the following: " + Arrays.toString(validGenres));
+            }
+        }
     }
 
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Verify the price isn't negative and is greater than $0
+     * @param price
+     */
     public void setPrice(double price) {
-        this.price = price;
+        if (price <= 0){
+            throw new IllegalArgumentException("Price must be greater than 0");
+        }else{
+            this.price = price;
+        }
     }
 
     public Image getImage() {
