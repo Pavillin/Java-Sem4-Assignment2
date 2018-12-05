db297ae819097d91bfc3b9ecda2311bc906b1885
Dylan <dylansprague11@hotmail.com>
2018-12-05 16:23:58 -0500

    GUI Setup

--- src/StoreViewController.java
+++ src/StoreViewController.java
@@ -1,2 +1,21 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
+
 public class StoreViewController {
+
+    @FXML private ListView<?> ListView;
+    @FXML private ImageView ImageView;
+    @FXML private Button SellButton;
+    @FXML private ComboBox<?> ComboBox;
+    @FXML private RadioButton RadioButtonHL;
+    @FXML private RadioButton RadioButtonLH;
+    @FXML private RadioButton RadioButtonAZ;
+    @FXML private RadioButton RadioButtonZA;
+    @FXML private Label LabelInvValue;
+    @FXML private Label LabelCatValue;
 }
