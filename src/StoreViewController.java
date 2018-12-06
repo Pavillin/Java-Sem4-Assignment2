ac02a9056eac255e40919f15c7f0e2036d422d5c
Dylan <dylansprague11@hotmail.com>
2018-12-06 11:52:27 -0500

    Added ability to sort products

--- src/StoreViewController.java
+++ src/StoreViewController.java
@@ -3,14 +3,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 public class StoreViewController implements Initializable {
 
-    @FXML private ListView<?> ListView;
+    @FXML private ListView<Product> ListView;
     @FXML private ImageView ImageView;
     @FXML private Button SellButton;
-    @FXML private ComboBox<?> ComboBox;
+    @FXML private ComboBox<String> ComboBox;
     @FXML private RadioButton RadioButtonHL;
     @FXML private RadioButton RadioButtonLH;
     @FXML private RadioButton RadioButtonAZ;
@@ -45,6 +47,11 @@ public class StoreViewController implements Initializable {
                 })
         );
 
+        //Sort the products list when a radio button is selected
+        sortToggleGroup.selectedToggleProperty().addListener(
+                (observable, oldValue, newValue) -> sortProducts()
+        );
+
         Inventory.setProductTreeMap();
         updateViewWithSelectedProduct();
     }
@@ -64,5 +71,71 @@ public class StoreViewController implements Initializable {
     public void updateViewWithSelectedProduct() {
         Product product = (Product) ListView.getSelectionModel().getSelectedItem();
         ImageView.setImage(product.getImage());
+        sortProducts();
+    }
+
+    /**
+     * Sort the products in the list based on the selected radio button
+     */
+    public void sortProducts(){
+        ArrayList<Product> sorted;
+        if (RadioButtonAZ.isSelected()) {
+            if (ComboBox.getValue() == null) {
+                sorted = Inventory.getProducts();
+                Collections.sort(sorted);
+                ListView.getItems().clear();
+                ListView.getItems().addAll(sorted);
+            } else {
+                String selectedGenre = ComboBox.getValue();
+                sorted = Inventory.getProductsInGenre(selectedGenre);
+                Collections.sort(sorted);
+                ListView.getItems().clear();
+                ListView.getItems().addAll(sorted);
+            }
+        }
+        if (RadioButtonZA.isSelected()) {
+            if (ComboBox.getValue() == null) {
+                sorted = Inventory.getProducts();
+                Collections.sort(sorted, Collections.reverseOrder());
+                ListView.getItems().clear();
+                ListView.getItems().addAll(sorted);
+            } else {
+                String selectedGenre = ComboBox.getValue();
+                sorted = Inventory.getProductsInGenre(selectedGenre);
+                Collections.sort(sorted, Collections.reverseOrder());
+                ListView.getItems().clear();
+                ListView.getItems().addAll(sorted);
+            }
+        }
+        if (RadioButtonHL.isSelected()) {
+            if (ComboBox.getValue() == null) {
+                sorted = Inventory.getProducts();
+                Collections.sort(sorted, (a,b) -> (a.getPrice() < b.getPrice()) ? 1 : -1);
+                ListView.getItems().clear();
+                ListView.getItems().addAll(sorted);
+            } else {
+                String selectedGenre = ComboBox.getValue();
+                sorted = Inventory.getProductsInGenre(selectedGenre);
+                Collections.sort(sorted, (a,b) -> (a.getPrice() < b.getPrice()) ? 1 : -1);
+                ListView.getItems().clear();
+                ListView.getItems().addAll(sorted);
+            }
+        }
+        if (RadioButtonLH.isSelected()) {
+            if (ComboBox.getValue() == null) {
+                sorted = Inventory.getProducts();
+                Collections.sort(sorted, (a,b) -> (a.getPrice() > b.getPrice()) ? 1 : -1);
+                ListView.getItems().clear();
+                ListView.getItems().addAll(sorted);
+            } else {
+                String selectedGenre = ComboBox.getValue();
+                sorted = Inventory.getProductsInGenre(selectedGenre);
+                Collections.sort(sorted, (a,b) -> (a.getPrice() > b.getPrice()) ? 1 : -1);
+                ListView.getItems().clear();
+                ListView.getItems().addAll(sorted);
+            }
+        }
+
+        initListView();
     }
 }
