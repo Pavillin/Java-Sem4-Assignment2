bb33073c5fe58ca077e1079fe4700cbcf77bf2f7
Dylan <dylansprague11@hotmail.com>
2018-12-08 17:41:14 -0500

    Added more products

--- src/Inventory.java
+++ src/Inventory.java
@@ -16,14 +16,21 @@ public class Inventory {
     }
     public ArrayList getProducts(){
         Product product1 = new Product("Rocket League", "E", "Sports", 29.99, 7, new Image("Images/rocketleague.jpg"));
-        Product product2 = new Product("Grand Theft Auto V", "M", "Action", 29.99, 4, new Image("Images/gta5.jpg"));
-        Product product3 = new Product("Red Dead Redemption 2", "M", "Action-Adventure", 79.99, 10, new Image("Images/rdr2.jpg"));
-        Product product4 = new Product("Firewatch", "M", "Adventure", 14.99, 9, new Image("Images/firewatch.jpg"));
-        Product product5 = new Product("Skyrim", "M", "Role-Playing", 19.99, 3, new Image("Images/skyrim.png"));
-        Product product6 = new Product("Arma3", "M", "Simulation", 39.99, 11, new Image("Images/a3.jpg"));
-        Product product7 = new Product("Civilization VI", "E10+", "Strategy", 59.99, 5, new Image("Images/civvi.jpg"));
-        Product product9 = new Product("ZrcheAge", "M", "MMO", 8.99, 11, new Image("Images/archeage.jpg"));
-        Product product8 = new Product("ArcheAge", "M", "MMO", 0, 11, new Image("Images/archeage.jpg"));
+        Product product2 = new Product("Madden 19", "E", "Sports", 89.99, 6, new Image("Images/madden19.jpeg"));
+        Product product3 = new Product("Grand Theft Auto V", "M", "Action", 29.99, 4, new Image("Images/gta5.jpg"));
+        Product product4 = new Product("No Man's Sky", "T", "Action", 66.49, 9, new Image("Images/nomanssky.jpg"));
+        Product product5 = new Product("Red Dead Redemption 2", "M", "Action-Adventure", 79.99, 10, new Image("Images/rdr2.jpg"));
+        Product product6 = new Product("The Last Of Us", "M", "Action-Adventure", 19.99, 3, new Image("Images/tlou.jpg"));
+        Product product7 = new Product("Firewatch", "M", "Adventure", 14.99, 9, new Image("Images/firewatch.jpg"));
+        Product product8 = new Product("Minecraft", "E10+", "Adventure", 35.99, 10, new Image("Images/Minecraft.jpg"));
+        Product product9 = new Product("Skyrim", "M", "Role-Playing", 19.99, 3, new Image("Images/skyrim.png"));
+        Product product10 = new Product("Fallout3", "M", "Role-Playing", 9.99, 5, new Image("Images/fallout3.png"));
+        Product product11 = new Product("Arma3", "M", "Simulation", 39.99, 11, new Image("Images/a3.jpg"));
+        Product product12 = new Product("Cities: Skylines", "E", "Simulation", 29.99, 11, new Image("Images/citiesskylines.jpg"));
+        Product product13 = new Product("Civilization VI", "E10+", "Strategy", 59.99, 5, new Image("Images/civvi.jpg"));
+        Product product14 = new Product("XCOM 2", "T", "Strategy", 79.99, 6, new Image("Images/xcom2.jpg"));
+        Product product15 = new Product("ArcheAge", "M", "MMO", 0, 11, new Image("Images/archeage.jpg"));
+        Product product16 = new Product("RuneScape", "T", "MMO", 0, 8, new Image("Images/rs.jpg"));
 
         ArrayList<Product> products = new ArrayList<>();
         products.add(product1);
@@ -33,8 +40,15 @@ public class Inventory {
         products.add(product5);
         products.add(product6);
         products.add(product7);
-        products.add(product9);
         products.add(product8);
+        products.add(product9);
+        products.add(product10);
+        products.add(product11);
+        products.add(product12);
+        products.add(product13);
+        products.add(product14);
+        products.add(product15);
+        products.add(product16);
         return products;
     }
 
